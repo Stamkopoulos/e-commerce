54e19800c293ca8b3f3f5818c2d57559b09b1cb9
G.Tzaris <https://github.com/tzarhs>
2025-12-03 15:23:03 +0200

    init(frontend) Initial Home page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header (){
+    return (
+        <div class='w-full bg-orange-500'>
+            <div class='text-2xl'>VOUTS ECOMMERCE</div>
+        </div>
+        )
+}
+
+export default Header
